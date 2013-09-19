f108d7587baeddd9f6257fe24ce5882e8b190bfb
Pradeep S <pradeep@Pradeeps-MacBook-Pro.local>
2013-09-19 21:03:14 +0530

    Coding style corrected

--- allocation_list.c
+++ allocation_list.c
@@ -1,8 +1,8 @@
 /*
  *  TRACEMALLOC
- * 
+ *
  *  Author: Pradeep Subrahmanion
- * 
+ *
  *  Date  :  1/1/2012
  */
 
@@ -17,7 +17,7 @@
 //
 
 /*
- *   function to get string from integer without using any inbuilt function 
+ *   function to get string from integer without using any inbuilt function
  */
 
 char *tochar(int i, char *p)
@@ -28,7 +28,7 @@ char *tochar(int i, char *p)
         *p = '\0';
         return p;
     }
-
+    
     p = tochar(i / 10, p);
     *p++ = i % 10 + '0';
     *p = '\0';
@@ -36,14 +36,14 @@ char *tochar(int i, char *p)
 }
 
 /*
- *   function to get length of a string  without using any inbuilt function 
+ *   function to get length of a string  without using any inbuilt function
  */
 
 static int wrapper_strlen (const char *str)
 {
-  const char *p;
-  for (p = str; *p; p++);
-  return p - str;
+    const char *p;
+    for (p = str; *p; p++);
+    return p - str;
 }
 
 //////////////////////////////////////////////////////////////////////////////////////////////
@@ -53,8 +53,8 @@ static int wrapper_strlen (const char *str)
 
 struct allocation_node
 {
-  struct allocation_node *link;
-  struct allocation *allocation_data;
+    struct allocation_node *link;
+    struct allocation *allocation_data;
 };
 struct allocation_node *first_node = NULL;
 
@@ -63,73 +63,73 @@ struct allocation_node *first_node = NULL;
  */
 void add_allocation_node(void * address,int line_no,size_t size,char *file_name,const char *function_name)
 {
-  struct allocation_node *temp =  (struct allocation_node *) sbrk(sizeof(struct allocation_node));
-  if(first_node != NULL) {
-    temp->link = first_node;
-  } else {
-    temp->link = NULL;
-  }
-
-  /*
-   * If list already contains allocation with same line number , add size to it.
-   */
-  struct allocation_node *it = first_node;
-  while (it != NULL) {	  
-  	if(it->allocation_data->line_number == line_no) {
-    	it->allocation_data->size += size;
-        return;
-    }    
-    it = it->link;
-  }
-
-  temp->allocation_data = (struct allocation *) sbrk(sizeof(struct allocation));
-  temp->allocation_data->address = address;
-  temp->allocation_data->line_number = line_no;
-  temp->allocation_data->size = size;
-  temp->allocation_data->file_name = file_name;
-  temp->allocation_data->function_name = function_name;
-  first_node = temp;
+    struct allocation_node *temp =  (struct allocation_node *) sbrk(sizeof(struct allocation_node));
+    if(first_node != NULL) {
+        temp->link = first_node;
+    } else {
+        temp->link = NULL;
+    }
+    
+    /*
+     * If list already contains allocation with same line number , add size to it.
+     */
+    struct allocation_node *it = first_node;
+    while (it != NULL) {
+        if(it->allocation_data->line_number == line_no) {
+            it->allocation_data->size += size;
+            return;
+        }
+        it = it->link;
+    }
+    
+    temp->allocation_data = (struct allocation *) sbrk(sizeof(struct allocation));
+    temp->allocation_data->address = address;
+    temp->allocation_data->line_number = line_no;
+    temp->allocation_data->size = size;
+    temp->allocation_data->file_name = file_name;
+    temp->allocation_data->function_name = function_name;
+    first_node = temp;
 }
 
 /*
- *  Remove an allocation from list 
+ *  Remove an allocation from list
  */
 
 void remove_allocation_node(void * address)
 {
-  struct allocation_node *it = first_node; 
-  struct allocation_node *prev_node = NULL;
-  while (it != NULL) {
+    struct allocation_node *it = first_node;
+    struct allocation_node *prev_node = NULL;
+    while (it != NULL) {
         void * _address = it->allocation_data->address;
         if(_address == address) {
         	
-        	if(prev_node == NULL) {       	  
+        	if(prev_node == NULL) {
         		first_node = it->link;
-        	} else {         
+        	} else {
         		prev_node->link = it->link;
-        	}        	
-          break;
+        	}
+            break;
         }
         prev_node = it;
-        it = it->link; 
-  }
+        it = it->link;
+    }
 }
 
 /*
  * This method writes allocation to given file descriptor
- * 
+ *
  */
 
 void write_to_file(int fd, struct allocation_node *it)
 {
 	char *mem = sbrk(8);
-    char *size = sbrk(8);           
+    char *size = sbrk(8);
     tochar(it->allocation_data->line_number,mem);
-    tochar(it->allocation_data->size,size);	     
+    tochar(it->allocation_data->size,size);
     write(fd,it->allocation_data->file_name,wrapper_strlen(it->allocation_data->file_name));
     write(fd,"\t",1);
     write(fd,it->allocation_data->function_name,wrapper_strlen(it->allocation_data->function_name));
-    write(fd,"\t\t",2);             
+    write(fd,"\t\t",2);
     write(fd,mem,wrapper_strlen(mem));
     write(fd,"\t",1);
     write(fd,size,wrapper_strlen(size));
@@ -137,22 +137,22 @@ void write_to_file(int fd, struct allocation_node *it)
 }
 
 /*
- *  This method reports leaked blocks.This method generates 'log.txt' file 
- *  
+ *  This method reports leaked blocks.This method generates 'log.txt' file
+ *
  */
 
 void report_allocations()
 {
-   int fd = open("log.txt",O_CREAT |O_RDWR );
-   struct allocation_node *it = first_node;
-   struct allocation_node *prev_node = NULL;
-   write(fd,"FILE\t\tFUNCTION\tLINENO\tSIZE\n--------------------------------------------\n",72);
-   write(1,"FILE\t\tFUNCTION\tLINENO\tSIZE\n--------------------------------------------\n",72);
-   while (it != NULL) {	   
+    int fd = open("log.txt",O_CREAT |O_RDWR );
+    struct allocation_node *it = first_node;
+    struct allocation_node *prev_node = NULL;
+    write(fd,"FILE\t\tFUNCTION\tLINENO\tSIZE\n--------------------------------------------\n",72);
+    write(1,"FILE\t\tFUNCTION\tLINENO\tSIZE\n--------------------------------------------\n",72);
+    while (it != NULL) {
         write_to_file(fd,it); /* write to log.txt */
 	    write_to_file(1,it);  /* write to stdout */
         it=it->link;
-     }
-   close(fd);
+    }
+    close(fd);
 }
 
