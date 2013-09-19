0f505b6335458c451aa87da87693cbbfb02de533
Pradeep S <pradeep@Pradeeps-MacBook-Pro.local>
2013-09-19 21:05:31 +0530

    Coding style corrected

--- tm_malloc.c
+++ tm_malloc.c
@@ -1,8 +1,8 @@
 /*
  *  TRACEMALLOC
- * 
+ *
  *  Author: Pradeep Subrahmanion
- * 
+ *
  *  Date  :  1/1/2012
  */
 #define _GNU_SOURCE
@@ -32,26 +32,26 @@ bool       registered_exit ;
 void tm_update_data(int line_number,const char *function_name,char *file_name)
 {
     tm_file_name =(char *) sbrk(strlen(file_name)-3); /*remove _tm from file name*/
-	strncpy(tm_file_name,file_name,strlen(file_name)-5);
-	strcat(tm_file_name,".c");
+    strncpy(tm_file_name,file_name,strlen(file_name)-5);
+    strcat(tm_file_name,".c");
     tm_line_number 	 = line_number;
     tm_function_name     = function_name;
 }
 
 /*
  * TraceMalloc implementation of standard library function 'malloc'
- * 
+ *
  */
 
 void * malloc(size_t size)
 {
-	void *ptr = dlmalloc(size);  /*allocate memory */    
+    void *ptr = dlmalloc(size);  /*allocate memory */
     add_allocation_node(ptr,tm_line_number,size,tm_file_name,tm_function_name); /*register allocation*/
-	if(!registered_exit) {
-	   atexit(report_allocations); /*at exit of the program , report leaked blocks*/
-	   registered_exit = true;
+    if(!registered_exit) {
+        atexit(report_allocations); /*at exit of the program , report leaked blocks*/
+        registered_exit = true;
     }
-   return ptr;
+    return ptr;
 }
 
 /*
@@ -60,12 +60,12 @@ void * malloc(size_t size)
 
 void free(void * ptr)
 {
-  remove_allocation_node(ptr); /* remove allocation from register*/
-  dlfree(ptr);        /* deallocate memory */
+    remove_allocation_node(ptr); /* remove allocation from register*/
+    dlfree(ptr);        /* deallocate memory */
 }
 
-/* 
- * This method calls free from  libc 
+/*
+ * This method calls free from  libc
  * NOT USED NOW
  */
 #if 0
@@ -74,11 +74,11 @@ void  std_free(void * ptr)
     char *error;
     void  (*std_free_ptr)(void *);
     dlerror();
-   *(void **)(&std_free_ptr) = dlsym(RTLD_NEXT,"free");
+    *(void **)(&std_free_ptr) = dlsym(RTLD_NEXT,"free");
     if ((error = dlerror()) != NULL) {
         fprintf(stderr, "symbol - %s\n", error);
         exit(EXIT_FAILURE);
-    }  
+    }
     (*std_free_ptr)(ptr);
 }
 #endif
